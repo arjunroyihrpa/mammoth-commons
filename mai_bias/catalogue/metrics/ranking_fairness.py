966cce2e2170f14cbda4b30267c844cec130d4bf
maniospas <maniospas@hotmail.com>
2025-05-14 22:37:59 +0300

    fixing some numpy installation issues

--- mai_bias/catalogue/metrics/ranking_fairness.py
+++ mai_bias/catalogue/metrics/ranking_fairness.py
@@ -648,7 +648,15 @@ def plot_network(
     return enc_str
 
 
-@metric(namespace="mammotheu", version="v0042", python="3.12", packages=("seaborn", "matplotlib", ))
+@metric(
+    namespace="mammotheu",
+    version="v0042",
+    python="3.12",
+    packages=(
+        "seaborn",
+        "matplotlib",
+    ),
+)
 def exposure_distance_comparison(
     dataset: Graph_CSH,
     model: ResearcherRanking,
