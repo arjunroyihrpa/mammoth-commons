d5bd44f89bd9f35e1d208d2de951953d83e1836c
Arjun Roy <arjunroy@Arjuns-MacBook-Pro.local>
2025-06-10 15:05:00 +0200

    removed ipynb checkpoints

--- mammoth_commons/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# from mammoth import models
-# from mammoth import exports
-# from mammoth import datasets
-# from mammoth import externals
-# from mammoth.integration import loader, metric
-# from mammoth import integration_callback
